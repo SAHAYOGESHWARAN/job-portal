af0caf5df4c6c81c6fa76c98557465eba2fa430d
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-12 08:17:27 +0530

    add job-portal master

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ import colors from "colors";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect
+    (process.env.MONGO_URL);
     console.log(
       `Connected To Mongodb Database ${mongoose.connection.host}`.bgMagenta
         .white
