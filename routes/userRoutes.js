2a268234a833901ba5ec416b0abb31e9e9671418
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-12 08:37:05 +0530

    add job - portal maaster

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,3 +3,13 @@ import { updateUserController } from "../controllers/userController.js";
 import userAuth from "../middelwares/authMiddleware.js";
 
 
+//router object
+const router = express.Router();
+
+//routes
+// GET USERS || GET
+
+// UPDATE USER || PUT
+router.put("/update-user", userAuth, updateUserController);
+
+export default router;
