0b91e33488d4e00f4fd35bf7f86b5aba62b4197b
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-12 10:00:08 +0530

    add job -portal master

--- models/jobsModel.js
+++ models/jobsModel.js
@@ -16,7 +16,23 @@ const jobSchema = new mongoose.Schema(
       enum: ["pending", "reject", "interview"],
       default: "pending",
     },
-    
+    workType: {
+      type: String,
+      enum: ["full-time", "part-time", "internship", "contaract"],
+      default: "full-time",
+    },
+    workLocation: {
+      type: String,
+      default: "Mumbai",
+      required: [true, "Work location is required"],
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
 
 export default mongoose.model("Job", jobSchema);
