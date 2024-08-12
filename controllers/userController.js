e4864c9490cdd53055452c1ef6592671dcf822fd
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-12 09:33:08 +0530

    add job-portal master

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,3 +7,16 @@ export const updateUserController = async (req, res, next) => {
   }
 
   
+  const user = await userModel.findOne({ _id: req.user.userId });
+  user.name = name;
+  user.lastName = lastName;
+  user.email = email;
+  user.location = location;
+
+  await user.save();
+  const token = user.createJWT();
+  res.status(200).json({
+    user,
+    token,
+  });
+};
