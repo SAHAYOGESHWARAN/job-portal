a92f8426428a0cd62fe117a36e55494b90cbd8f5
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-12 10:08:36 +0530

    add job-portal master

--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -12,3 +12,20 @@ import userAuth from "../middelwares/authMiddleware.js";
 const router = express.Router();
 
 
+//routes
+// CREATE JOB || POST
+router.post("/create-job", userAuth, createJobController);
+
+//GET JOBS || GET
+router.get("/get-job", userAuth, getAllJobsController);
+
+//UPDATE JOBS ||  PATCH
+router.patch("/update-job/:id", userAuth, updateJobController);
+
+//DELETE JOBS || DELETE
+router.delete("/delete-job/:id", userAuth, deleteJobController);
+
+// JOBS STATS FILTER || GET
+router.get("/job-stats", userAuth, jobStatsController);
+
+export default router;
